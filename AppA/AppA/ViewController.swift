9c926d3755efad387ff80dfd8af84d0bff6a526b
brunov0id <bruno@belab.com.br>
2016-05-10 09:55:44 -0300

    added AppA and AppB

---
+++ AppA/AppA/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AppA
+//
+//  Created by Bruno da Luz on 5/9/16.
+//  Copyright © 2016 Bruno da Luz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func actionCallApp(sender: AnyObject) {
+        
+    }
+
+}
+
---
+++ AppB/AppB/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  AppB
+//
+//  Created by Bruno da Luz on 5/9/16.
+//  Copyright © 2016 Bruno da Luz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func actionCallApp(sender: AnyObject) {
+        let customURL: String = "AppA.scheme://?token=123&register=1"
+        self.schemeAvaible(customURL)
+    }
+    
+    func schemeAvaible(scheme: String) {
+        let url = NSURL(string: scheme)
+        let canBeOpened = UIApplication.sharedApplication().canOpenURL(url!)
+        
+        if canBeOpened {
+            print("canBeOpened")
+            label.hidden = true
+            UIApplication.sharedApplication().openURL(url!)
+        } else {
+            print("cannot canBeOpened")
+            label.hidden = false
+        }
+    }
+}
+
